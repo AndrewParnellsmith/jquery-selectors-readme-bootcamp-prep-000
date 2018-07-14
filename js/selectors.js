9d8afb8d228eeff3c8afbdacd28badb333ef40e7
Andrew Parnell-Smith <andrewparnellsmith@gmail.com>
2018-07-14 05:42:55 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -9,7 +9,7 @@ function ninjaBabySelector() {
   return $('#baby-ninja')
 }
 function divSelector() {
-  return ('div .pics')
+  return ('.pic')
 }
 function firstListItem() {
  return $('ul li:first-child')
